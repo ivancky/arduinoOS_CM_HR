68f15c156a5951318a192750263c75f7af50c9aa
ivankcy <ivan.chewky@gmail.com>
2018-05-07 17:42:54 +0800

    Added 9 color channels Misc code changes

--- src/Color Mixing + Heart Rate.ino
+++ src/Color Mixing + Heart Rate.ino
@@ -1,23 +1,45 @@
 #include <U8g2lib.h>
 #include <avr/pgmspace.h>
-#include <AltSoftSerial.h>
-#include "RunningAverage.h"
-// #include <HM12.h>
+// #include <AltSoftSerial.h>
+#include <SoftPWM.h>
+#include <SoftwareSerial.h>
+// #include "RunningAverage.h"
 
-AltSoftSerial BlueBee; // permanently pins 8,9
+/* The following example demonstrates setting channels for all pins
+   on the ATmega328P or ATmega168 used on Arduino Uno, Pro Mini,
+   Nano and other boards. */
+SOFTPWM_DEFINE_CHANNEL(0, DDRD, PORTD, PORTD0);  //Arduino pin 0
+SOFTPWM_DEFINE_CHANNEL(1, DDRD, PORTD, PORTD1);  //Arduino pin 1
+SOFTPWM_DEFINE_CHANNEL(2, DDRD, PORTD, PORTD2);  //Arduino pin 2
+SOFTPWM_DEFINE_CHANNEL(3, DDRD, PORTD, PORTD3);  //Arduino pin 3
+SOFTPWM_DEFINE_CHANNEL(4, DDRD, PORTD, PORTD4);  //Arduino pin 4
+SOFTPWM_DEFINE_CHANNEL(5, DDRD, PORTD, PORTD5);  //Arduino pin 5
+SOFTPWM_DEFINE_CHANNEL(6, DDRD, PORTD, PORTD6);  //Arduino pin 6
+SOFTPWM_DEFINE_CHANNEL(7, DDRD, PORTD, PORTD7);  //Arduino pin 7
+// SOFTPWM_DEFINE_CHANNEL(8, DDRB, PORTB, PORTB0);  //Arduino pin 8
+// SOFTPWM_DEFINE_CHANNEL(9, DDRB, PORTB, PORTB1);  //Arduino pin 9
+SOFTPWM_DEFINE_CHANNEL(10, DDRB, PORTB, PORTB2);  //Arduino pin 10
+SOFTPWM_DEFINE_CHANNEL(11, DDRB, PORTB, PORTB3);  //Arduino pin 11
+SOFTPWM_DEFINE_CHANNEL(12, DDRB, PORTB, PORTB4);  //Arduino pin 12
+SOFTPWM_DEFINE_CHANNEL(13, DDRB, PORTB, PORTB5);  //Arduino pin 13
+SOFTPWM_DEFINE_CHANNEL(14, DDRC, PORTC, PORTC0);  //Arduino pin A0
+SOFTPWM_DEFINE_CHANNEL(15, DDRC, PORTC, PORTC1);  //Arduino pin A1
+SOFTPWM_DEFINE_CHANNEL(16, DDRC, PORTC, PORTC2);  //Arduino pin A2
+SOFTPWM_DEFINE_CHANNEL(17, DDRC, PORTC, PORTC3);  //Arduino pin A3
+SOFTPWM_DEFINE_CHANNEL(18, DDRC, PORTC, PORTC4);  //Arduino pin A4
+SOFTPWM_DEFINE_CHANNEL(19, DDRC, PORTC, PORTC5); //Arduino pin A5
 
-/* Constructor */
-U8G2_SSD1327_SEEED_96X96_1_HW_I2C u8g2(U8G2_R0);
-// U8G2_SSD1327_SEEED_96X96_1_SW_I2C u8g2(U8G2_R0, A3, A2);
-// U8G2_SSD1327_SEEED_96X96_F_HW_I2C u8g2(U8G2_R0);
 
-// unsigned int delayTime = 1000;
+SoftwareSerial BlueBee(8,9); // permanently pins 8,9
+
+/* Here you make an instance of desired channel counts you want
+   with the default 256 PWM levels (0 ~ 255). */
+SOFTPWM_DEFINE_OBJECT(20);
+
 const int numReadings = 1;
-const long interval = 5000;            // timeout for dust sensor
-const long automatic_interval = 6000; // timeout for automatic mode
-RunningAverage myRA(100);
+// const long interval = 5000;            // timeout for dust sensor
+// const long automatic_interval = 6000; // timeout for automatic mode
 
-// SoftwareSerial Dusty(8,9); // RX, TX
 byte data[6];   // for incoming serial data
 byte readbuffer[2];
 int rb = 0;
@@ -28,78 +50,194 @@ int total = 0;                            // the running total
 int averageHeartRate = 0;                 // the average
 unsigned long previousMillis = 0;         // will store last time LED was updated
 unsigned long currentMillis = 0;
-int j = 0, k = 0;
+int i = 0, j = 0, k = 0;
 int step = 50;
 
-// dust sensing
-int measurePin = A1;
-int ledPower = A0;
-int voltage_threshold = 2;
-unsigned int samplingTime = 280;
-unsigned int deltaTime = 40;
-unsigned int sleepTime = 9680;
-unsigned int delayTime = 5000;
-float voMeasured = 0;
-float calcVoltage = 0;
-float dustDensity = 0;
-bool dust_indicator = false;
-bool status = false;
-bool automaticMode = false;
-int mistState = 0;
-unsigned long previousMillis2 = automatic_interval;
-float previous_brightness = 0;
-float brightness = 0;
+// pin declarations
+int DeepBlue  =   2;
+int Blue      =   3;
+int Green     =   4;
+int Lime      =   5;
+int Yellow    =   6;
+int Amber     =   7;
+int Red       =   10;
+int White1    =   11;
+int White2    =   12;
 
 // everything else
-int ALSPin = A4;
-int fanPin = 4;
-int button1 = 12;
-int button2 = 7;
-int button3 = 11;
-int mist = 3;
-int warmLED = 6, coolLED = 5;
-int btnAutomatic = 0, btnFan = 0, btnMist = 0;
-int brightness_multiplier = 5; // base brightness is level 5. Min = 1, max = 10
+int colorcontrolMode = 1;
 
 void setup() {
-  u8g2.begin();
-  do {
-      u8g2.setFont(u8g2_font_logisoso78_tn);
-      u8g2.setCursor(0, 87);
-      u8g2.print(averageHeartRate);
-    } while ( u8g2.nextPage() );
   BlueBee.begin(9600); // set baudrate according to BlueBee 4.0 baudrate
   BlueBee.println("BlueBee ready");
-  pinMode(ledPower, OUTPUT); // Initialize infrared LED in dust sensor
-  pinMode(fanPin, OUTPUT);
-  pinMode(warmLED, OUTPUT);
-  pinMode(coolLED, OUTPUT);
-  pinMode(mist, OUTPUT);
-  pinMode(button1, INPUT_PULLUP);
-  pinMode(button2, INPUT_PULLUP);
-  pinMode(button3, INPUT_PULLUP);
-  pinMode(ALSPin, INPUT);
+  Serial.begin(9600);
+
+  Palatis::SoftPWM.begin(120);
+  resetColors();
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     heartRateIndex[thisReading] = 0; // initialize all the readings to 0:
   }
-  Serial.begin(9600);
-  Brighten(warmLED, 0, 25 * brightness_multiplier, 5);
-  status = false;
-  myRA.clear(); // explicitly start clean
-  previous_brightness = 25 * brightness_multiplier;
 }
 
 void loop(){
-  // ReceiveHeartData();
-  // DisplayInfo_Screen();
-  // analogWrite(coolLED, 255);
-  // analogWrite(warmLED, 255);
-  // Mist_On();
-  CalculateDust();
-  DisplayInfo_Dust();
-  Read_Buttons();
-  // Brightness_Automatic();
-  // Streetlight();
+  readBluetooth();
+  ColorControl();
+  // ReceiveHeartData
+}
+
+void ColorControl(){
+  while (readbuffer[0] == 170) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(DeepBlue, i);
+    i = 0;
+    break;
+  }
+  while (readbuffer[0] == 187) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(Blue, i);
+    i = 0;
+    break;
+  }
+  while (readbuffer[0] == 204) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(Green, i);
+    i = 0;
+    break;
+  }
+  while (readbuffer[0] == 221) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(Lime, i);
+    i = 0;
+    break;
+  }
+  while (readbuffer[0] == 238) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(Yellow, i);
+    i = 0;
+    break;
+  }
+  while (readbuffer[0] == 255) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(Amber, i);
+    i = 0;
+    break;
+  }
+  while (readbuffer[0] == 160) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(Red, i);
+    i = 0;
+    break;
+  }
+  while (readbuffer[0] == 176) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(White1, i);
+    i = 0;
+    break;
+  }
+  while (readbuffer[0] == 192) {
+    Serial.print(readbuffer[0]);
+    Serial.print(", ");
+    Serial.println(readbuffer[1]);
+    i = readbuffer[1];
+    for (int j = 0; j < 2; j++) {
+      readbuffer[j] = 0;
+      delay(1);
+    }
+    setColor(White2, i);
+    i = 0;
+    break;
+  }
+}
+
+void readBluetooth(){
+  while (BlueBee.available() > 0) {
+    int sb = BlueBee.read();
+    delay(10);
+    readbuffer[rb] = sb;
+    rb++;
+    if (rb == 2) {
+      rb = 0;
+      break;
+    }
+  }
+}
+
+void setColor(int _Color, int _PWM){
+  if (_PWM < 0){
+    _PWM = 0;
+  }
+  if (_PWM > 255){
+    _PWM = 255;
+  }
+  Palatis::SoftPWM.set(_Color, _PWM);
+}
+
+void resetColors(){
+  setColor(DeepBlue, 0);
+  setColor(Blue, 0);
+  setColor(Green, 0);
+  setColor(Lime, 0);
+  setColor(Yellow, 0);
+  setColor(Amber, 0);
+  setColor(Red, 0);
+  setColor(White1, 0);
+  setColor(White2, 0);
 }
 
 void ReceiveHeartData(){
@@ -123,240 +261,11 @@ void ReceiveHeartData(){
   break;
   }
   // heart rate range from 50 ---> 135
-  if(averageHeartRate < 60){averageHeartRate = 60;}
-  if(averageHeartRate > 100){averageHeartRate = 100;}
-  j = (averageHeartRate - 60) * 6;
-  k = (100 - averageHeartRate) * 6;
-  analogWrite(warmLED, j);
-  analogWrite(coolLED, k);
-  delay(1);
-}
-
-void CalculateDust(){
-  digitalWrite(ledPower, LOW);
-  delayMicroseconds(samplingTime); // pulse infrared LED
-  voMeasured = analogRead(measurePin);
-  delayMicroseconds(deltaTime); // slight delay to preserve data integrity
-  digitalWrite(ledPower, HIGH);
-  delayMicroseconds(sleepTime);
-  calcVoltage = voMeasured * (5.0 / 1024);
-  dustDensity = 0.17 * calcVoltage - 0.1;
-  if (dustDensity < 0)
-  {
-    dustDensity = 0.00;
-  }
-}
-
-void DisplayInfo_Dust(){
-  if(calcVoltage > voltage_threshold){
-    currentMillis = millis();
-    dust_indicator = true;
-    Serial.println("WARNING! FOG DETECTED");
-  }
-  if (millis() - currentMillis >= interval){ // if no dust sensed for Tinterval, switch off
-    dust_indicator = false;
-  }
-  // Serial.print("Raw Signal (0-1023): ");
-  // Serial.print(voMeasured);
-  // Serial.print(" - Voltage: ");
-  // Serial.print(calcVoltage);
-  // Serial.print(" - Dust Density: ");
-  // Serial.println(dustDensity);
-}
-
-void DisplayInfo_Screen(){
-  u8g2.firstPage(); // display on OLED
-  do {
-    if(averageHeartRate < 100){ // 3 numbers or more
-      u8g2.setFont(u8g2_font_logisoso78_tn);
-      u8g2.setCursor(0, 87);
-    }
-    else{ // 2 numbers or less
-      u8g2.setFont(u8g2_font_logisoso50_tn);
-      u8g2.setCursor(0, 73);
-    }
-    u8g2.setFontDirection(0); // top to bottom
-    u8g2.print(averageHeartRate);
-  } while ( u8g2.nextPage() );
-  delay(1);
-}
-
-// void Draw_Screensaver(){
-//   // u8g2.clearDisplay();
-//   u8g2.firstPage(); // display on OLED
-//   do{
-//     u8g2.drawXBM(0,0,u8g_logo_width, u8g_logo_height, u8g_logo_bits);
-//     delay(1);
-//   } while( u8g2.nextPage());
-// }
-
-void Fan_On(){
-  digitalWrite(fanPin, HIGH);
-}
-
-void Fan_Off(){
-  digitalWrite(fanPin, LOW);
-}
-
-void Mist_On(){
-  digitalWrite(mist, HIGH);
-}
-
-void Mist_Off(){
-  digitalWrite(mist, LOW);
-}
-
-void Read_Buttons(){
-  btnAutomatic = digitalRead(button1);
-  btnMist = digitalRead(button2);
-  btnFan = digitalRead(button3);
-
-  // if (btnAutomatic == LOW && automaticMode == false){ // automatic mode
-  if (btnMist == HIGH && btnFan == HIGH && automaticMode == false) { //
-    Fan_Off();
-    Mist_Off();
-    mistState = 0;
-    previousMillis2 = millis() - automatic_interval;
-    automaticMode = true;
-  }
-
-  if (automaticMode == true && btnAutomatic == HIGH){ //  autobrightness
-    Brightness_Automatic();
-  }
-  if (automaticMode == true && btnAutomatic == LOW){ //  auto mist
-    Streetlight_Automatic();
-  }
-
-  if (btnMist == LOW || btnFan == LOW && automaticMode == true) { // manual mode
-    Fan_Off();
-    Mist_Off();
-    automaticMode = false;
-  }
-  if(automaticMode == false){
-    Streetlight_Manual();
-    if (btnMist == LOW){
-      Mist_On();
-    } else { Mist_Off(); }
-    if (btnFan == LOW){
-      Fan_On();
-    } else { Fan_Off(); }
-  }
-}
-
-void Streetlight_Manual(){
-  if (dust_indicator == true && status == false){
-    Brighten(coolLED, 0, 255, 5);
-    // analogWrite(warmLED, 0);
-    // Dim(warmLED, 150, 50, 5);
-  }
-  if (dust_indicator == false && status == true){
-    Dim(coolLED, 255, 0, 5);
-    // Brighten(warmLED, 0, 100, 5);
-    // Brighten(warmLED, 50, 150, 5);
-  }
-}
-
-void Streetlight_Automatic(){
-  if (dust_indicator == true && status == false){
-    Brighten(coolLED, 0, 255, 5);
-    // analogWrite(warmLED, 0);
-    // Dim(warmLED, 150, 50, 5);
-  }
-  if (dust_indicator == false && status == true){
-    Dim(coolLED, 255, 0, 5);
-    // Brighten(warmLED, 0, 100, 5);
-    // Brighten(warmLED, 50, 150, 5);
-  }
-  unsigned long currentMillis2 = millis();
-  if (currentMillis2 - previousMillis2 >= automatic_interval) {
-    previousMillis2 = currentMillis2;
-    if (mistState == 0) {
-      mistState = 1;
-      Mist_On();
-      Fan_Off();
-    }
-    else if (mistState == 1) {
-      mistState = 2;
-      Mist_Off();
-    }
-    else  {
-      mistState = 0;
-      Fan_On();
-    }
-  }
-}
-
-bool Brighten(int _color, int _z, int _max, int _speed){ // LED string, start, end, speed
-  for(_z; _z <= _max; _z++){
-    if(_z < 0){_z = 0;}
-    if(_z > 255){_z = 255;}
-    analogWrite(_color, _z);
-    delay(_speed);
-  }
-  return status = true;
-}
-
-bool Dim(int _color, int _z, int _min, int _speed){ // LED string, start, end, speed
-  for(_z; _z >= _min; _z--){
-    if(_z < 0){_z = 0;}
-    if(_z > 255){_z = 255;}
-    analogWrite(_color, _z);
-    delay(_speed);
-  }
-  return status = false;
-}
-
-void Brightness_Automatic(){
-  mistState = 0;
-  previousMillis2 = millis() - automatic_interval;
-  Mist_Off();
-  Fan_Off();
-  float readBrightness = analogRead(ALSPin);
-  myRA.addValue(readBrightness);
-  delay(10);
-
-  if (myRA.getAverage() < 100) {
-    brightness_multiplier = 10;
-    brightness = 25 * brightness_multiplier;
-  }
-  else if (myRA.getAverage() >= 150 && myRA.getAverage() < 300) {
-    brightness_multiplier = 8;
-    brightness = 25 * brightness_multiplier;
-
-  }
-  else if (myRA.getAverage() >= 350 && myRA.getAverage() < 500) {
-    brightness_multiplier = 6;
-    brightness = 25 * brightness_multiplier;
-
-  }
-  else if (myRA.getAverage() >= 550 && myRA.getAverage() < 700) {
-    brightness_multiplier = 4;
-    brightness = 25 * brightness_multiplier;
-
-  }
-  else if (myRA.getAverage() >= 750 && myRA.getAverage() < 900) {
-    brightness_multiplier = 2;
-    brightness = 25 * brightness_multiplier;
-
-  }
-  else if (myRA.getAverage() >= 950) {
-    brightness_multiplier = 0;
-    // brightness = 25 * brightness_multiplier;
-    brightness = 0;
-
-  }
-  while (brightness < previous_brightness){
-    Dim (warmLED, previous_brightness, brightness, 15);
-    if(status == false){ // loop finished
-      break;
-    }
-  }
-  while (brightness > previous_brightness){
-    Brighten(warmLED, previous_brightness, brightness, 15);
-    if(status == true){ // loop finished
-      break;
-    }
-  }
-  previous_brightness = brightness;
-  // analogWrite(warmLED, 25 * brightness_multiplier);
+  // if(averageHeartRate < 60){averageHeartRate = 60;}
+  // if(averageHeartRate > 100){averageHeartRate = 100;}
+  // j = (averageHeartRate - 60) * 6;
+  // k = (100 - averageHeartRate) * 6;
+  // analogWrite(warmLED, j);
+  // analogWrite(coolLED, k);
+  // delay(1);
 }
